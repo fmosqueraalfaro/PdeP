a5ae37d9e3653ed74c11165300ac1f1fc0d15fc0
fmosqueraalfaro <fmosqueraalfaro@frba.utn.edu.ar>
2023-06-01 21:51:35 -0300

    Quiero terminar el tp

--- Prueba.hs
+++ Prueba.hs
@@ -35,9 +35,6 @@ type Accion = Jugador -> Jugador
 precioPropiedad :: Propiedad -> Int
 precioPropiedad unaPropiedad = snd unaPropiedad
 
-pasarPorElBanco :: Accion
-pasarPorElBanco unJugador = cambiarTacticaACompradorCompulsivo . aumentaDinero 40 $ unJugador
-
 aumentaDinero ::  Int -> Jugador  -> Jugador
 aumentaDinero unDinero alguienQueJuega = alguienQueJuega {cantDinero = cantDinero alguienQueJuega + unDinero}
 
@@ -47,35 +44,14 @@ disminuyeDinero unDinero unJugador = unJugador {cantDinero = cantDinero unJugado
 cambiarTacticaACompradorCompulsivo :: Jugador -> Jugador
 cambiarTacticaACompradorCompulsivo  unJugador = unJugador {tactica = "Comprador compulsivo"}
 
-enojarse :: Accion
-enojarse unJugador = aumentaDinero 50 . agregaAccion gritar $ unJugador
 
 agregaAccion :: Accion -> Jugador -> Jugador
 agregaAccion unaAccion unJugador = unJugador {acciones = acciones unJugador ++ [unaAccion]}
 
-gritar :: Accion
-gritar unJugador = agregaAlNombre "AHHH" unJugador
 
 agregaAlNombre :: String -> Jugador -> Jugador
 agregaAlNombre unaPalabra unJugador = unJugador{nombre= unaPalabra ++ nombre unJugador}
 
-
-pagarAAccionistas :: Accion
-pagarAAccionistas unJugador
-    | tactica unJugador == "Accionista" = aumentaDinero 200 unJugador
-    | otherwise = disminuyeDinero 100 unJugador
-
-{-subastar :: Accion
-subastar unJugador = undefined
-
-puedenGanarPropiedad :: Jugador -> Bool
-puedenGanarPropiedad unJugador = tactica unJugador == "Oferente singular" || tactica unJugador == "Accionista"
-
-subastar: al momento de una subasta solo quienes tengan como tácticas “Oferente singular” o “Accionista” podrán ganar la propiedad. 
-Ganar implica restar el precio de la propiedad de su dinero y sumar la nueva adquisición a sus propiedades.
-
-ganarPropiedad :: Accion
--}
 propiedadBarata :: Propiedad -> Bool
 propiedadBarata unaPropiedad = precioPropiedad unaPropiedad < 150
 
@@ -84,4 +60,26 @@ precioAlquiler unaPropiedad
     | propiedadBarata unaPropiedad = 10
     | otherwise = 20
 
+pasarPorElBanco :: Accion
+pasarPorElBanco unJugador = cambiarTacticaACompradorCompulsivo . aumentaDinero 40 $ unJugador
 
+enojarse :: Accion
+enojarse unJugador = aumentaDinero 50 . agregaAccion gritar $ unJugador
+
+gritar :: Accion
+gritar unJugador = agregaAlNombre "AHHH" unJugador
+
+subastar :: Propiedad -> Accion
+subastar unaPropiedad unJugador 
+    | tactica unJugador == "Oferente singular" || tactica unJugador == "Accionista" = unJugador{cantDinero = cantDinero unJugador - precioPropiedad unaPropiedad, propiedadesCompradas = propiedadesCompradas unJugador ++ [unaPropiedad]}
+    | otherwise = unJugador
+
+
+pagarAAccionistas :: Accion
+pagarAAccionistas unJugador
+    | tactica unJugador == "Accionista" = aumentaDinero 200 unJugador
+    | otherwise = disminuyeDinero 100 unJugador
+
+{-cobrarAlquileres :: Accion
+cobrarAlquileres unJugador = 
+-}
